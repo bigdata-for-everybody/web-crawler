fa46c2971daecf69811bb3ba83ebb869d9cecee1
drielnox <bl4z3r@gmail.com>
2017-06-29 21:34:31 -0300

    leo una pagina

--- WebCrawler/src/WebCrawler/Program.cs
+++ WebCrawler/src/WebCrawler/Program.cs
@@ -1,15 +1,34 @@
-﻿namespace WebCrawlerBigDataForEverybody.WebCrawler
+﻿// <copyright file="Program.cs" company="Big Data for Everybody">
+// Copyright (c) Big Data for Everybody. All rights reserved.
+// </copyright>
+
+namespace WebCrawlerBigDataForEverybody.WebCrawler
 {
+    using HtmlAgilityPack;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    class Program
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var webget = new HtmlWeb();
+            var htmldoc = webget.Load("http://www.cuantogano.com/");
+
+            var a = htmldoc.DocumentNode.Descendants()
+                .Where(x => x.Name == "div")
+                .Where(x => x.Attributes.Any())
+                .Where(x => x.Attributes["class"]);
+
+            Console.Write(a);
+
+            Console.ReadLine();
         }
     }
 }
